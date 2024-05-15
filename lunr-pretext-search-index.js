82c4b5a33baf84808d4828b3251850d2f921303a
laurennelsen <77991089+laurennelsen@users.noreply.github.com>
2024-05-15 21:11:26 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -37,6 +37,69 @@ var ptx_lunr_docs = [
   "body": " Types of Data  Text of section.  "
 },
 {
+  "id": "sec-Random-Variables",
+  "level": "1",
+  "url": "sec-Random-Variables.html",
+  "type": "Section",
+  "number": "3.1",
+  "title": "Random Variables",
+  "body": " Random Variables  Say some stuff about random variables in general....    Two Types of Random Variables:  discrete random variable: outcomes are whole numbers after performing an experiment  continuous random variable: outcomes are any numerical value (including decimals or fractions) after performing an experiment      ...insert examples of these types of random variables..... ....Random variables can be distributed differently....    A discrete probability distribution is a listing of all possible outcomes of an experiment for a discrete random variable along with the relative frequency of each outcome   Each outcome in the distribution must be mutually exclusive     for all ; i.e., the probability of each outcome is between 0 and 1     ; the sum of all probabilities is 1       "
+},
+{
+  "id": "def-RandomVariables",
+  "level": "2",
+  "url": "sec-Random-Variables.html#def-RandomVariables",
+  "type": "Definition",
+  "number": "3.1.1",
+  "title": "",
+  "body": "  Two Types of Random Variables:  discrete random variable: outcomes are whole numbers after performing an experiment  continuous random variable: outcomes are any numerical value (including decimals or fractions) after performing an experiment     "
+},
+{
+  "id": "def-discrete-probability-distribution",
+  "level": "2",
+  "url": "sec-Random-Variables.html#def-discrete-probability-distribution",
+  "type": "Definition",
+  "number": "3.1.2",
+  "title": "",
+  "body": "  A discrete probability distribution is a listing of all possible outcomes of an experiment for a discrete random variable along with the relative frequency of each outcome   Each outcome in the distribution must be mutually exclusive     for all ; i.e., the probability of each outcome is between 0 and 1     ; the sum of all probabilities is 1      "
+},
+{
+  "id": "sec-Desc-Stats-Discrete",
+  "level": "1",
+  "url": "sec-Desc-Stats-Discrete.html",
+  "type": "Section",
+  "number": "3.2",
+  "title": "Descriptive Statistics for a Discrete Probability Distribution",
+  "body": " Descriptive Statistics for a Discrete Probability Distribution  ....insert stuff about things we might want to ask about a distribution.... what average value would we expect rolling a die or playing a gambling game? how spread out are the values...?      The mean, or expected value of a discrete probability distribution is the weighted average of all outcomes of the random variable.     The expected monetary value is the mean when the discrete random variable is in dollars ($)    The variance of a discrete probability distribution measures the spread of the individual values around the mean.     The standard deviation of a discrete probability distribution measures the dispersion of the outcome of the random variable in relation to the mean.        (Donnely 5.7) The table below shows the discrete probability distribution for the number of bedrooms per house in a certain community:    of Bedrooms  Probability    3  0.23    4  0.57    5  0.14    6  ???      Determine the missing probability for a 6-bedroom house.  Determine the mean number of bedrooms per house.  Determine the standard deviation for the number of bedrooms per house. (Hint: Use the table below to compute the variance first....fix this or add something in Excel.....)                                                  It turns out we can calculate this in Excel! ...insert Excel commands....   (Donnelly 5.45)  Tees R Us manufactures and sells T-shirts for sporting events, is providing shirts for an upcoming tournament. Each shirt will cost to produce and will be sold for . Any unsold shirts at the end of the tournament can be sold for a piece in the near future. Tees R Us assumes the demand for the shirts will be , , , or . They also estimate that the probabilities of each of these sales levels occurring will be , , , and , respectively. Determine the expected monetary value of the project if Tees R Us chooses to print shirts for the tournament.   "
+},
+{
+  "id": "sec-Desc-Stats-Discrete-3",
+  "level": "2",
+  "url": "sec-Desc-Stats-Discrete.html#sec-Desc-Stats-Discrete-3",
+  "type": "Definition",
+  "number": "3.2.1",
+  "title": "",
+  "body": "    The mean, or expected value of a discrete probability distribution is the weighted average of all outcomes of the random variable.     The expected monetary value is the mean when the discrete random variable is in dollars ($)    The variance of a discrete probability distribution measures the spread of the individual values around the mean.     The standard deviation of a discrete probability distribution measures the dispersion of the outcome of the random variable in relation to the mean.      "
+},
+{
+  "id": "sec-Desc-Stats-Discrete-4",
+  "level": "2",
+  "url": "sec-Desc-Stats-Discrete.html#sec-Desc-Stats-Discrete-4",
+  "type": "Example",
+  "number": "3.2.2",
+  "title": "",
+  "body": " (Donnely 5.7) The table below shows the discrete probability distribution for the number of bedrooms per house in a certain community:    of Bedrooms  Probability    3  0.23    4  0.57    5  0.14    6  ???      Determine the missing probability for a 6-bedroom house.  Determine the mean number of bedrooms per house.  Determine the standard deviation for the number of bedrooms per house. (Hint: Use the table below to compute the variance first....fix this or add something in Excel.....)                                                 "
+},
+{
+  "id": "sec-Desc-Stats-Discrete-6",
+  "level": "2",
+  "url": "sec-Desc-Stats-Discrete.html#sec-Desc-Stats-Discrete-6",
+  "type": "Example",
+  "number": "3.2.4",
+  "title": "",
+  "body": " (Donnelly 5.45)  Tees R Us manufactures and sells T-shirts for sporting events, is providing shirts for an upcoming tournament. Each shirt will cost to produce and will be sold for . Any unsold shirts at the end of the tournament can be sold for a piece in the near future. Tees R Us assumes the demand for the shirts will be , , , or . They also estimate that the probabilities of each of these sales levels occurring will be , , , and , respectively. Determine the expected monetary value of the project if Tees R Us chooses to print shirts for the tournament.  "
+},
+{
   "id": "backmatter-2",
   "level": "1",
   "url": "backmatter-2.html",
